7c6d590faf0f26a54d5c279f84b24d561c7d18f4
Mariano Capiliku <marianocapiliku@gmail.com>
2024-12-04 00:15:42 +0100

    Applying react-hook form in create new transaction form

--- src/components/NewTransactionForm.tsx
+++ src/components/NewTransactionForm.tsx
@@ -1,31 +1,65 @@
+import { useForm } from "react-hook-form";
 import NewTransactionType from "./NewTransactionType";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const NewTransactionFormSchema = z.object({
+  description: z.string(),
+  price: z.number(),
+  category: z.string(),
+  // type: z.enum(["income", "outcome"]),
+});
+
+type NewTransactionFormInputs = z.infer<typeof NewTransactionFormSchema>;
 
 const NewTransactionForm = () => {
+  const {
+    handleSubmit,
+    register,
+    formState: { isSubmitting },
+  } = useForm<NewTransactionFormInputs>({
+    resolver: zodResolver(NewTransactionFormSchema),
+  });
+
+  async function handlerSubmiteNewTransaction(data: NewTransactionFormInputs) {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.log(data);
+  }
+
   return (
-    <form className="mt-8 flex flex-col gap-4 ">
+    <form
+      onSubmit={handleSubmit(handlerSubmiteNewTransaction)}
+      className="mt-8 flex flex-col gap-4 "
+    >
       <input
         type="text"
         placeholder="Description"
         required
         className="rounded-md bg-gray-900 text-gray-300 p-4 outline-none focus:outline-green-500 "
+        {...register("description")}
       />
       <input
         type="number"
         placeholder="Price"
         required
         className="rounded-md bg-gray-900 text-gray-300 p-4 outline-none focus:outline-green-500 "
+        {...register("price", {
+          valueAsNumber: true,
+        })}
       />
       <input
         type="text"
         placeholder="Category"
         required
         className="rounded-md bg-gray-900 text-gray-300 p-4 outline-none focus:outline-green-500"
+        {...register("category")}
       />
 
       <NewTransactionType />
       <button
+        disabled={isSubmitting}
         type="submit"
-        className="h-14 bg-green-500 text-white font-bold py-0 px-5 rounded-md outline-none mt-6 focus:outline-green-500 hover:brightness-90"
+        className="h-14 bg-green-500 text-white font-bold py-0 px-5 rounded-md outline-none mt-6 focus:outline-green-500 hover:brightness-90 disabled:cursor-not-allowed disabled:opacity-60"
       >
         {" "}
         Create{" "}
--- src/components/NewTransactionModal.tsx
+++ src/components/NewTransactionModal.tsx
@@ -6,7 +6,10 @@ const NewTransactionModal = () => {
   return (
     <Dialog.Portal>
       <Dialog.Overlay className="fixed w-full min-h-dvh inset-0 bg-black/75" />
-      <Dialog.Content className="fixed min-w-[32rem]  rounded-md py-10 px-12 bg-gray-800 top-2/4 left-2/4  -translate-y-2/4 -translate-x-2/4   ">
+      <Dialog.Content
+        aria-describedby={undefined}
+        className="fixed min-w-[32rem]  rounded-md py-10 px-12 bg-gray-800 top-2/4 left-2/4  -translate-y-2/4 -translate-x-2/4   "
+      >
         <Dialog.Close className="absolute top-6 right-6 ">
           <X size={22} className="text-gray-500      hover:text-red-400" />
         </Dialog.Close>
